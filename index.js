292396e19545eabdc546d1d0a59d171406bbee66
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2024-07-20 17:16:12 +0530

    user and video model with utils

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  }),
+);
 
 // Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static("public"));
 
 // Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI, {})
   .then(() => {
-    console.log("Connected to MongoDB");
+    console.log("Connected to MongoDB!!" );
   })
   .catch((err) => {
     console.error("Failed to connect to MongoDB", err);
