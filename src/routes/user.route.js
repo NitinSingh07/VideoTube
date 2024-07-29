aa5fc5c3bbbe434a30da0f36365321221a953dd0
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2024-07-29 12:23:36 +0530

    Add getWatchHistory endpoint

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updateAccountDetails,
+  updateUserCoverImage,
 } from "../controllers/user.controller.js";
 import { upload } from "./../middleware/multer.middleware.js";
 import { verifyJWT } from "./../middleware/auth.middleware.js";
@@ -21,5 +27,17 @@ router.route("/register").post(
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("cover-image"), updateUserCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
 
+// /c/: is used because we used params in the controllers in getUserChannelProfile
 export default router;
